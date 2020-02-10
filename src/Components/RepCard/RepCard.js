967489c5a7f934fd1dacc8b2242ded1643ee2606
Maikalanimai <kalonekamimoto@gmail.com>
2020-02-10 11:25:59 -0700

    funcitonal d3 state map

--- src/Components/RepCard/RepCard.js
+++ src/Components/RepCard/RepCard.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 
 const RepCard = () => {
   return <div>RepCard</div>;
--- src/Components/Representative/Representatives.js
+++ src/Components/Representative/Representatives.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 
 const Representatives = () => {
   return <div>Representatives</div>;
