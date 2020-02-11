b8694a3ca066ed8b344d915161f04a115fd22033
Maikalanimai <kalonekamimoto@gmail.com>
2020-02-11 16:09:26 -0700

    functioning county map

--- src/Components/CountyMap/CountyMap.js
+++ src/Components/CountyMap/CountyMap.js
@@ -1,76 +1,70 @@
-import React, {useRef, useEffect, useState} from "react";
-import {select, geoPath, geoAlbersUsa} from 'd3'
-import useResizeObserver from '../../hooks/useResizeObserver.js'
-import counties from '../../assets/countiesSmall.json'
-import states from '../../assets/states.json'
-import stateNumbers from './stateNumber.js'
-import {connect} from 'react-redux'
+import React, { useRef, useEffect, useState } from "react";
+import { select, geoPath, geoAlbersUsa } from "d3";
+import useResizeObserver from "../../hooks/useResizeObserver.js";
+import counties from "../../assets/countiesSmall.json";
+import states from "../../assets/states.json";
+import stateNumbers from "./stateNumber.js";
+import { connect } from "react-redux";
 
-const CountyMap = () => {
-  const svgRef = useRef()
-  const wrapperRef = useRef()
-  let dimensions = useResizeObserver(wrapperRef)
+const CountyMap = props => {
+  console.log(props);
+  const svgRef = useRef();
+  const wrapperRef = useRef();
+  let dimensions = useResizeObserver(wrapperRef);
   //change to null post testing
-  //! CHECK TO MAKE SURE 14 WORKS
-  const [selectedStateNum, setSelectedStateNum] = useState(null)
-  const selectedState = states.features.filter(feature => 
-    feature.properties.STATEFP == selectedStateNum)
-
-  // console.log(selectedState)
+  const [selectedStateNum, setSelectedStateNum] = useState("");
   useEffect(() => {
-    setSelectedStateNum( stateNumbers.reduce((acc, e )=> 
-      e.state=== usState ? acc = e.number : null
-    , '')
-    )
+    setSelectedStateNum(
+      stateNumbers.reduce(
+        (acc, e) => (e.state === props.usState ? acc + e.number : acc),
+        ""
+      )
+    );
+    const selectedState = states.features.filter(
+      feature => feature.properties.STATEFP === selectedStateNum
+    );
 
-    const svg = select(svgRef.current)
-    const reducedFeatures = counties.features.filter(feature => 
-      feature.properties.STATE == selectedStateNum
-    )
-    let {width, height} = 
-      dimensions || wrapperRef.current.getBoundingClientRect()
+    console.log(selectedState);
+
+    const svg = select(svgRef.current);
+    const reducedFeatures = counties.features.filter(
+      feature => feature.properties.STATE === selectedStateNum
+    );
+    let { width, height } =
+      dimensions || wrapperRef.current.getBoundingClientRect();
 
     const projection = geoAlbersUsa()
-      .fitSize(
-        [width, height],
-        selectedState[0]
-        )
-        .precision(2)
+      .fitSize([width, height], selectedState[0])
+      .precision(2);
 
-  const pathGenerator = geoPath().projection(projection)
+    const pathGenerator = geoPath().projection(projection);
 
-
-  //counties.features.properties.STATEFP = numerical id of state
-  //counties.features.properties.NAME = county name
-  // console.log(reducedFeatures)
-  svg
-    .selectAll('.county')
-    .data(reducedFeatures)
-    .join('path')
-    .attr('class', 'county')
-    // .attr('class', feature => selectedState === feature.properties.STATEFP ? 'selected-state' : 'not-selected-state')
-    .attr('id', feature => feature.properties.NAME)
-    .attr('d', feature => pathGenerator(feature))
-    
-
-  }, [selectedStateNum])
-  return (<div>
-
-    <div className='map-wrapper' ref={wrapperRef}>
-      <svg className='map' ref={svgRef}></svg>
+    //counties.features.properties.STATEFP = numerical id of state
+    //counties.features.properties.NAME = county name
+    // console.log(reducedFeatures)
+    svg
+      .selectAll(".county")
+      .data(reducedFeatures)
+      .join("path")
+      .attr("class", "county")
+      // .attr('class', feature => selectedState === feature.properties.STATEFP ? 'selected-state' : 'not-selected-state')
+      .attr("id", feature => feature.properties.NAME)
+      .attr("d", feature => pathGenerator(feature));
+  }, [selectedStateNum]);
+  return (
+    <div>
+      <div className="map-wrapper" ref={wrapperRef}>
+        <svg className="map" ref={svgRef}></svg>
       </div>
-  </div>);
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-  const {
-    city,
-    usState
-  }= state;
+  const { usState } = state;
   return {
-    city, 
     usState
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(CountyMap);
