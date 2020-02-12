c704d44d386d6b6c91a545a19598f8a503eda3e0
coopergoldenholt <coopergoldenholt12@gmail.com>
2020-02-12 10:27:46 -0700

    frontend can get profile picture from twitter

--- server/controllers/representativeContoller.js
+++ server/controllers/representativeContoller.js
@@ -1,26 +1,53 @@
 const axios = require("axios");
 const express = require("express");
-const { API_KEY } = process.env;
+const {
+	API_KEY,
+	CONSUMER_KEY,
+	CONSUMER_SECRET,
+	ACCESS_TOKEN,
+	TOKEN_SECRET
+} = process.env;
+var https = require("follow-redirects").https;
+var fs = require("fs");
+var Twit = require("twit");
+
+var T = new Twit({
+	consumer_key: CONSUMER_KEY,
+	consumer_secret: CONSUMER_SECRET,
+	access_token: ACCESS_TOKEN,
+	access_token_secret: TOKEN_SECRET
+});
 
 module.exports = {
-  getRepresentatives: async (req, res) => {
-    const { address } = req.query;
-    const representatives = await axios.get(
-      `https://www.googleapis.com/civicinfo/v2/representatives?key=${API_KEY}&address=${address}}`
-    );
-    req.session.address = {
-      address: req.query.address
-    };
+	getRepresentatives: async (req, res) => {
+		const { address } = req.query;
+		const representatives = await axios.get(
+			`https://www.googleapis.com/civicinfo/v2/representatives?key=${API_KEY}&address=${address}}`
+		);
+		req.session.address = {
+			address: req.query.address
+		};
 
-    res.status(200).send(representatives.data);
-  }
-  //   getRepsPicture: async (req, res) => {
-  //     const { handle } = req.query;
-  //     console.log(req.query);
-  //     const picture = await axios.get(
-  //       `https://twitter.com/realDonaldTrump/profile_image?size=original`
-  //     );
-  //     console.log(picture.data);
-  //     res.status(200).send(picture);
-  //   }
+		res.status(200).send(representatives.data);
+	},
+	getRepsPicture: async (req, res) => {
+		const { handle } = req.query;
+		T.get("users/show", { screen_name: `${handle}` }, function(
+			err,
+			data,
+			response
+		) {
+			let profileImage = data.profile_image_url_https;
+			if (profileImage) {
+				let profileImageTwo = profileImage
+					.split("_normal")
+					.toString()
+					.replace(",", "");
+
+				res.status(200).send(profileImageTwo);
+			} else {
+				res.status(200).send(null);
+			}
+		});
+	}
 };
--- src/Components/RepCard/RepCard.js
+++ src/Components/RepCard/RepCard.js
@@ -10,99 +10,99 @@ import { makeStyles } from "@material-ui/core";
 import Axios from "axios";
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 300
-  },
-  media: {
-    height: 25,
-    paddingTop: "20%"
-  }
+	root: {
+		maxWidth: 300
+	},
+	media: {
+		height: 25,
+		paddingTop: "20%"
+	}
 });
 
 const RepCard = props => {
-  const classes = useStyles();
-  // const [profilePicture, setProfilePicture] = useState("");
+	const classes = useStyles();
+	const [profilePicture, setProfilePicture] = useState("");
 
-  // useEffect(
-  //   () =>
-  //     props.person.socialMedia
-  //       ? Axios.get(
-  //           `/api/representatives/picture?handle=${props.person.socialMedia[0].id}`
-  //         ).then(res => {
-  //           console.log(res.data);
-  //           setProfilePicture(res.data);
-  //         })
-  //       : null,
-  //   [profilePicture]
-  // );
+	useEffect(
+		() =>
+			props.person.socialMedia
+				? Axios.get(
+						`/api/representatives/picture?handle=${props.person.socialMedia[0].id}`
+				  ).then(res => {
+						console.log(res.data);
+						setProfilePicture(res.data);
+				  })
+				: null,
+		[]
+	);
 
-  return (
-    <Card className={classes.root} elevation="5">
-      <CardActionArea>
-        {/* {props.person.socialMedia ? (
-          props.person.socialMedia[0].id === "Facebook" ? (
-            <CardMedia
-              className={classes.media}
-              image={`http://graph.facebook.com/${props.person.socialMedia[0].id}/picture?type=large`}
-              title="political profile image"
-            />
-          ) : (
-            <CardMedia
-              className={classes.media}
-              image={`https://twitter.com/${props.person.socialMedia[0].id}/profile_image?size=original`}
-              title="profile image"
-            />
-          )
-        ) : ( */}
-        <CardMedia
-          className={classes.media}
-          image={
-            "https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_960,f_auto/10140482_den8mp.png"
-          }
-          title="default image"
-        />
-        {/* )} */}
-      </CardActionArea>
+	return (
+		<Card className={classes.root} elevation="5">
+			<CardActionArea>
+				{props.person.socialMedia ? (
+					props.person.socialMedia[0].id === "Facebook" ? (
+						<CardMedia
+							className={classes.media}
+							image={profilePicture}
+							title="political profile image"
+						/>
+					) : (
+						<CardMedia
+							className={classes.media}
+							image={profilePicture}
+							title="profile image"
+						/>
+					)
+				) : (
+					<CardMedia
+						className={classes.media}
+						image={
+							"https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_960,f_auto/10140482_den8mp.png"
+						}
+						title="default image"
+					/>
+				)}
+			</CardActionArea>
 
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          {props.person.officalName}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {props.person.title}
-          <p>{props.person.party}</p>
-        </Typography>
-      </CardContent>
-      <Button size="medium" color="primary">
-        Learn More
-      </Button>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="h2">
+					{props.person.officalName}
+				</Typography>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{props.person.title}
+					<p>{props.person.party}</p>
+				</Typography>
+			</CardContent>
+			<Button size="medium" color="primary">
+				Learn More
+			</Button>
 
-      <CardContent>
-        {props.person.socialMedia ? (
-          <a
-            target="_blank"
-            rel="noopener"
-            href={`https://www.facebook.com/${props.person.socialMedia[0].id}`}
-          >
-            FB
-          </a>
-        ) : (
-          <div></div>
-        )}
-        {props.person.socialMedia ? (
-          <a
-            target="_blank"
-            rel="noopener"
-            href={`https://www.twitter.com/${props.person.socialMedia[0].id}`}
-          >
-            TWR
-          </a>
-        ) : (
-          <div></div>
-        )}
-      </CardContent>
-    </Card>
-  );
+			<CardContent>
+				{props.person.socialMedia ? (
+					<a
+						target="_blank"
+						rel="noopener"
+						href={`https://www.facebook.com/${props.person.socialMedia[0].id}`}
+					>
+						FB
+					</a>
+				) : (
+					<div></div>
+				)}
+				{props.person.socialMedia ? (
+					<a
+						target="_blank"
+						rel="noopener"
+						href={`https://www.twitter.com/${props.person.socialMedia[0].id}`}
+					>
+						TWR
+					</a>
+				) : (
+					<div></div>
+				)}
+			</CardContent>
+		</Card>
+	);
 };
 
 export default withRouter(RepCard);
