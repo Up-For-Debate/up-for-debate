cc5e1b0a6dcc3e9e2de59bfa2fd54ccda5dae283
Greg Kezos <greg.kezos@gmail.com>
2020-02-13 11:50:39 -0700

    register to vote is a button. Started building out database

---
+++ db/get_abbrev.sql
@@ -0,0 +1,3 @@
+select state_abbrev
+from states
+where us_state like $1
--- src/Components/RegisterToVote/RegisterToVote.js
+++ src/Components/RegisterToVote/RegisterToVote.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { connect } from 'react-redux'
+import { Paper, Grid, Button } from '@material-ui/core'
+import { withRouter } from 'react-router-dom'
 
-const RegisterToVote = () => {
-  return <div>RegisterToVote</div>;
+
+const RegisterToVote = (props) => {
+
+const checkState = (place) => {
+  switch(place){
+    case 'Utah':
+      return 'https://secure.utah.gov/voterreg/login.html?selection=REGISTER'
+    default:
+      return 'https://vote.gov/register/ar/'
+  }
+}
+
+  return (
+    <>
+      <Paper>
+        <Grid container style={{justifyContent:'center'}}>
+          <Grid item >
+            <Button 
+              variant="contained" 
+              color="secondary"
+
+            >
+              Register To Vote
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
+    )
 };
 
-export default RegisterToVote;
+
+
+const mapStateToProps = reduxState => {
+  return reduxState
+}
+
+export default withRouter(connect(mapStateToProps, {})(RegisterToVote));
