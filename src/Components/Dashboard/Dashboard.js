2cd900733afbb6528b0fade3e9badb4217154b3c
coopergoldenholt <coopergoldenholt12@gmail.com>
2020-02-14 14:12:21 -0700

    where to vote has an onclick, created new component wheretovotepopup

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,51 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 // import { HashLink as Link } from "react-router-hash-link";
+import WhereToVotePopup from "../Vote/WhereToVotePopup";
 import Representatives from "../Representative/Representatives";
 import CountyMap from "../CountyMap/CountyMap";
 import { Grid, Paper, Button } from "@material-ui/core";
 import { useTheme, makeStyles } from "@material-ui/core/styles";
-import RegisterToVote from '../RegisterToVote/RegisterToVote'
+import RegisterToVote from "../RegisterToVote/RegisterToVote";
 
 const Dashboard = () => {
-  const theme = useTheme();
-  const useStyles = makeStyles({
-    paper: {
-      height: 75,
-      display: "flex",
-      justifyContent: "space-evenly",
-      alignItems: "center"
-    }
-  });
-  const classes = useStyles();
+	const [displayPopup, setDisplayPopup] = useState(false);
+	const theme = useTheme();
+	const useStyles = makeStyles({
+		paper: {
+			height: 75,
+			display: "flex",
+			justifyContent: "space-evenly",
+			alignItems: "center"
+		}
+	});
+	const classes = useStyles();
+	console.log(displayPopup);
+	return (
+		<Grid
+			container
+			className="dashboard"
+			theme={theme}
+			style={{ backgroundColor: theme.palette.primary.light }}
+		>
+			<Grid item xs={12} className="link-area">
+				<Paper
+					theme={theme}
+					elevation={3}
+					style={{ backgroundColor: theme.palette.primary.dark }}
+					className={classes.paper}
+				>
+					<RegisterToVote />
+					<Button
+						variant="contained"
+						color="secondary"
+						onClick={() => setDisplayPopup(!displayPopup)}
+					>
+						Where to vote
+					</Button>
 
-  return (
-    <Grid
-      container
-      className="dashboard"
-      theme={theme}
-      style={{ backgroundColor: theme.palette.primary.light }}
-    >
-      <Grid item xs={12} className="link-area">
-        <Paper
-          theme={theme}
-          elevation={3}
-          style={{ backgroundColor: theme.palette.primary.dark }}
-          className={classes.paper}
-        >
-          <RegisterToVote />
-          <Button variant="contained" color="secondary">
-            Where to vote
-          </Button>
-        </Paper>
-      </Grid>
-      <Grid item xs={8} className="rep-area">
-        <Representatives />
-      </Grid>
-      <Grid item xs={4} className="map-area">
-        <CountyMap />
-      </Grid>
-    </Grid>
-  );
+					{displayPopup ? <WhereToVotePopup /> : null}
+				</Paper>
+			</Grid>
+			<Grid item xs={8} className="rep-area">
+				<Representatives />
+			</Grid>
+			<Grid item xs={4} className="map-area">
+				<CountyMap />
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Dashboard;
