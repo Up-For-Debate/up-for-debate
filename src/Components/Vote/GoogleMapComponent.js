322d52554bf717583bb211fb1cd9cb155085caf8
coopergoldenholt <coopergoldenholt12@gmail.com>
2020-02-14 12:25:54 -0700

    google maps renders, some voting information renders

---
+++ src/Components/Vote/GoogleMapComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+	GoogleMap,
+	LoadScript,
+	Marker,
+	OverlayView
+} from "@react-google-maps/api";
+const { REACT_APP_GOOGLE_MAPS_KEY } = process.env;
+
+const mapContainerStyle = {
+	height: "400px",
+	width: "800px"
+};
+
+const center = {
+	lat: 33.772,
+	lng: -117.214
+};
+
+const GoogleMapComponent = props => {
+	console.log(props.formatedLocation);
+	return (
+		<LoadScript id="script-loader" googleMapsApiKey={REACT_APP_GOOGLE_MAPS_KEY}>
+			<GoogleMap
+				id="example-map"
+				mapContainerStyle={mapContainerStyle}
+				zoom={12}
+				center={{ lat: props.lat, lng: props.lng }}
+			>
+				<Marker
+					position={{ lat: props.lat, lng: props.lng }}
+					clickable={true}
+					animation="bounce"
+				/>
+				<OverlayView
+					position={{ lat: props.lat + 0.035, lng: props.lng - 0.06 }}
+					mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+				>
+					<div
+						style={{
+							background: `white`,
+							border: `1px solid #ccc`,
+							padding: 15
+						}}
+					>
+						<h2>{props.formatedLocation}</h2>
+						<a
+							target="_blank"
+							rel="noopener"
+							href={`https://www.google.com/maps/search/${props.formatedLocation}/@${props.lat}${props.lng}`}
+						>
+							Get Directions!
+						</a>
+					</div>
+				</OverlayView>
+			</GoogleMap>
+		</LoadScript>
+	);
+};
+
+export default GoogleMapComponent;
