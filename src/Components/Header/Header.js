bea493360df06ada57da2a4ed09a051799cb9d3a
Greg Kezos <greg.kezos@gmail.com>
2020-02-17 09:23:48 -0700

    started adding drawer menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
-import { useTheme, makeStyles } from "@material-ui/core";
-import { Paper, Button } from "@material-ui/core";
+import { useTheme, makeStyles, IconButton } from "@material-ui/core";
+import { Paper, Button, Grid, Drawer } from "@material-ui/core";
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 
 const Header = () => {
+  const [drawerState, setDrawerState] = useState(false)
+
   const theme = useTheme();
   const useStyles = makeStyles({
     paper: {
@@ -15,25 +26,77 @@ const Header = () => {
       alignItems: "center",
       fontFamily: "Muli, sans-serif",
       position: "fixed"
+    },
+    list: {
+      width: 250
     }
   });
   const classes = useStyles();
+
+
+  const toggleDrawer =  () => {
+    setDrawerState(!drawerState)
+  }
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer()}
+      onKeyDown={toggleDrawer()}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+
   return (
+    <>
     <Paper className={classes.paper} id="header-paper" elevation="5">
-      <div className="header-home">
-        <Button id="home-button">
-          <Link>Home</Link>
-        </Button>
-      </div>
-      <div className="header-explore">
-        <Button id="explore">
-          <Link>Explore</Link>
-        </Button>
-        <Button>
-          <Link id="quiz">Quiz</Link>
-        </Button>
-      </div>
+      <Grid container spacing={3}>
+
+        <Grid item xs={3} sm={3} md={3} lg={3} xl={3} className="header-home">
+          <Button id="home-button">
+            <Link>Home</Link>
+          </Button>
+        </Grid>
+
+        <div className="header-explore">
+          <Button id="explore">
+            <Link>Explore</Link>
+          </Button>
+          <Button>
+            <Link id="quiz">Quiz</Link>
+          </Button>
+        </div>
+        <IconButton onClick={ toggleDrawer }>
+          <MenuRoundedIcon />
+        </IconButton>
+
+      </Grid>
     </Paper>
+    <div>
+    <Drawer anchor="right" open={drawerState} >
+        {sideList}
+      </Drawer>
+    </div>
+    </>
   );
 };
 
