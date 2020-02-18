339a7b0c59ec2cadd1ad574d7ccd0a65094ace09
Greg Kezos <greg.kezos@gmail.com>
2020-02-18 15:50:19 -0700

    unit testing done

--- src/__tests__/SearchInput.test.js
+++ src/__tests__/SearchInput.test.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react'
-import { render  } from '@testing-library/react'
+import React from 'react'
+import { render, fireEvent  } from '@testing-library/react'
 import store from '../redux/store'
 import { Provider } from 'react-redux'
-import axios from 'axios'
 import { HashRouter } from 'react-router-dom'
 import SearchInput from '../Components/SearchInput/SearchInput'
-
-import {handleSubmit, handleClose, handleSelect, setOpen } from '../Components/SearchInput/SearchInput'
 
 it('renders the search input', () => {
   const {container} = render(
@@ -19,6 +16,29 @@ it('renders the search input', () => {
   expect(container).not.toBe(null)
 })
 
-it('SearchInput will not allow submission without city and state', () => {
-  
+it(`SearchInput will originally display the string 'City, State'`, () => {
+  const {container}  = render(
+    <HashRouter>
+      <Provider store={store}>
+        <SearchInput />
+      </Provider>
+    </HashRouter>
+  )
+  expect(container.textContent).toContain('City, State')
+
 })
+
+it('SearchInput will not allow submission without city and state', () => {
+  const { getByTestId, container}  = render(
+    <HashRouter>
+      <Provider store={store}>
+        <SearchInput />
+      </Provider>
+    </HashRouter>
+  )
+  const button = getByTestId('submit-button')
+  fireEvent.click(button)
+  expect(container.textContent).toContain('Please input valid city')
+
+})
+
